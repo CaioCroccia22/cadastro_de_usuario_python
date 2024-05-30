ed938f78bc29161cd65aa2b42664e3b4597e469a
caiocroccia22 <caiocroccia22@gmail.com>
2024-05-29 21:25:08 -0300

    chore: creating treeView

--- gui.py
+++ gui.py
@@ -5,4 +5,12 @@ root = tk.Tk()
 
 root.title('cadastro de Alunos')
 
+# 1 - Criando tabela Treeview
+tree = ttk.Treeview(root, columns=('Name', 'Email'))
+tree.heading('Name', text='Name')
+tree.heading('Email', text='Email')
+tree.pack()
+
+
+
 root.mainloop()
