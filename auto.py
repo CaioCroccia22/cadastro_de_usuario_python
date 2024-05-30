7dda79ae7a9654325599a83cad66bb154425d7de
caiocroccia22 <caiocroccia22@gmail.com>
2024-05-30 08:41:07 -0300

    fix: remove startfile(path) from loop

--- auto.py
+++ auto.py
@@ -2,8 +2,12 @@ import pyautogui
 from time import sleep
 import os
 
-path = "C:\Users\Caio\Recursos\Documents\Programação\Python\Aplicação_cadastro_alunos\dist\gui.exe"
 
+path = r"C:\Users\Caio\Recursos\Documents\Programação\Python\Aplicação_cadastro_alunos\dist\gui.exe"
+if os.path.exists(path):
+    os.startfile(path)
+else:
+    print("O arquivo executável não foi encontrado.")
 
 with open('alunos.txt', 'r', encoding='UTF-8') as file:
     for line in file:
@@ -11,9 +15,6 @@ with open('alunos.txt', 'r', encoding='UTF-8') as file:
         # print(line.split(',')[1])
         aluno = line.split(',')[0]
         email = line.split(',')[1]
-        if os.path.exists(path):
-            os.startfile(path)
-        else:
-            print("O arquivo executável não foi encontrado.")
+        
         
         
