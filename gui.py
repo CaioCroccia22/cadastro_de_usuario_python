736d6749204bec0b09f6ad67610cb84b75ddbc9d
caiocroccia22 <caiocroccia22@gmail.com>
2024-05-29 21:41:43 -0300

    feat: add labels, entrys and  button

--- gui.py
+++ gui.py
@@ -11,6 +11,22 @@ tree.heading('Name', text='Name')
 tree.heading('Email', text='Email')
 tree.pack()
 
+#  Criando os campos
+
+label_name = tk.Label(root, text='Name')
+label_name.pack()
+entry_name = tk.Entry(root)
+entry_name.pack()
+
+label_email = tk.Label(root, text='Email')
+label_email.pack()
+entry_email = tk.Entry(root)
+entry_email.pack()
+
+
+# Botão para adicionar User
+butto_add = tk.Button(root, text='Adicionar Aluno')
+butto_add.pack()
 
 
 root.mainloop()
