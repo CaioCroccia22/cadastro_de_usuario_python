f27fd19d5ec17732e8ad56ed1b64f722a28dcdd9
caiocroccia22 <caiocroccia22@gmail.com>
2024-05-30 08:48:40 -0300

    feat: writing aluno and email from alunos.txt

--- auto.py
+++ auto.py
@@ -16,6 +16,8 @@ with open('alunos.txt', 'r', encoding='UTF-8') as file:
         aluno = line.split(',')[0]
         email = line.split(',')[1]
         pyautogui.click(304,314, duration=3)
+        pyautogui.write(aluno)
         pyautogui.click(355,350, duration=3)
+        pyautogui.write(email)
         pyautogui.click(338,378, duration=3)
         
